64c2b93ff81369ce53e974186cf050656cf6eb31
Tobe O <thosakwe@gmail.com>
2018-06-14 18:07:04 -0400

    include/ dir

---
+++ include/pubbydb.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2018, Tobechukwu Osakwe.
+//
+// All rights reserved.
+//
+// Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file.
+#ifndef PUBBYDB_MONO_REPO_PUBBYDB_HPP
+#define PUBBYDB_MONO_REPO_PUBBYDB_HPP
+
+#endif //PUBBYDB_MONO_REPO_PUBBYDB_HPP
